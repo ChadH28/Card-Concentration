3db0c226fc4f9e0c18c92064c3f7efc3c8de9adf
ChadH28 <chadhoosain@gmail.com>
2021-09-29 15:39:23 +0200

    Worked on card select and flip

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,58 +1,200 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="game-section">
+        <header>
+            <h1>Card Concentration</h1>
+            <div class="float-right">
+                <button type="button" class="btn reset">
+              <i class="fa fa-repeat"></i>
+              </button>
+                <button type="button" class="btn btn-danger">
+                <i class="fa fa-sign-out"></i>
+                </button>
+            </div>
+        </header>
+    
+        <section class="score-panel">
+    
+            <span class="moves">0 Moves</span>
+    
+        </section>
+    
+        <div div class="container">
+            <div class="deck row row-cols-6 row-cols-md-6 row-cols-lg-6">
+                <!-- v-if="card.value1 === card.value2 && card.suit('DAIMOND') === card.suit('HEARTS') && card.suit('CLUB') === card.suit('SPADE')" -->
+                <div class="col" v-for="(card,index) in cards" :key="index" :value="card.value" @click="toggleAlert()">
+                    <div class="card-content" @click="toggleFlip()" :class="{ flipped: isActive}">
+                        <div class="card-front">
+    
+                        </div>
+                        <div class="card-back">
+                            <img class="img-fluid card-image" :src="card.image" />
+                            <p>{{ card.value}}</p>
+                            <p>{{ card.suit }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: "Concentration",
+    data() {
+        return {
+            player1: null,
+            player2: null,
+            card: null,
+            moves: null,
+            cards: [""],
+            isActive: false,
+        };
+    },
+    created() {
+        this.getDeck();
+    },
+    methods: {
+        toggleAlert() {
+            alert("selected", this.cards)
+        },
+        toggleFlip() {
+            this.isActive = !this.isActive;
+            console.log(this.cards.value)
+        },
+        // getting deck of cards images from the deckofcardsapi
+        getDeck() {
+            const url =
+                "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+            fetch(url, {
+                    mode: "cors", //if FE and BE are on different hosts/url
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                })
+                .then(response => response.json())
+                .then(json => {
+                    //API response gets returned
+                    // console.log(json);
+                    // console.log(json.deck_id);
+                    const deckID = json.deck_id;
+                    const url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52`;
+                    fetch(url, {
+                            mode: "cors",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        })
+                        .then(response => response.json())
+                        .then(json => {
+                            // console.log(json.cards)
+                            this.cards = json.cards;
+                        });
+                });
+        },
+    },
+};
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.game-section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding-top: 60px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+h1 {
+    font-family: "Open Sans", sans-serif;
+    font-weight: 300;
+    color: white;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.reset {
+    background-color: orange;
+    color: white;
+    margin-right: 5px;
 }
-a {
-  color: #42b983;
+
+/*
+ * Styles for the Score Panel
+ */
+
+.score-panel {
+    text-align: center;
+    width: 660px;
+    padding-left: 32px;
+    padding-right: 32px;
+    margin-bottom: 10px;
 }
+
+.score-panel {
+    float: left;
+    margin: 0;
+    padding: 0;
+    display: inline-block;
+    margin: 0 5px 0 0;
+}
+
+.score-panel {
+    list-style: none;
+    display: inline-block;
+}
+
+.moves {
+    padding-left: 5px;
+    float: left;
+}
+
+
+/*
+ * Styles for the deck of cards
+ */
+
+.deck {
+    align-items: center;
+    width: 670px;
+    min-height: 680px;
+    padding: 32px;
+    border-radius: 8px;
+    backdrop-filter: blur(16px) saturate(180%);
+    -webkit-backdrop-filter: blur(16px) saturate(180%);
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    background-color: rgba(255, 255, 255, 0.4);
+    margin: 20px auto;
+}
+
+.card-image {
+    margin-bottom: 10px;
+    background: linear-gradient(160deg, #3c384d 0%, #ffffff 450%);
+    border-radius: 6px;
+    border: white 3px solid;
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    animation: flash-light-gry 0.3s;
+}
+
+.card-content {
+    cursor: pointer;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.card-front,
+.card-back {
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+}
+
+.card-back {
+    transform: rotateY(180deg);
+}
+
+.flipped {
+    transform: rotateY(180deg);
+}
+
 </style>

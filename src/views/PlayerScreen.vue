b668dd0629aa5a60fa64abad258785d0dc677626
ChadH28 <chadhoosain@gmail.com>
2021-10-02 15:29:58 +0200

    Worked on welcome screen welcome modal -- used props to pass input values to display player name

--- src/views/PlayerScreen.vue
+++ src/views/PlayerScreen.vue
@@ -1,158 +1,115 @@
 <template>
-  <div class="player-section">
-    <div class="container">
-      <header class="screen-header">
-        <h2>Are you ready to play?</h2>
-      </header>
-      <div class="row ">
-        <div class="col-sm-6 ">
-          <div class="player-container">
-            <div class="card-body image-container">
-              <img
-                class="card-img-top img-fluid player-img"
-                src="../assets/spacer1.png"
-              />
+    <div class="player-section">
+        <div class="container">
+            <header class="screen-header">
+                <h2>Are you ready to play?</h2>
+            </header>
+            <div class="row ">
+              <!-- {{player1.name}} {{player2.name}} -->
+                <div class="col-sm-6 ">
+                    <div class="player-container">
+                        <div class="card-body image-container">
+                            <img class="card-img-top img-fluid player-img" src="../assets/spacer1.png" />
+                        </div>
+                        <div class="footer">
+                            <input type="text" v-model="player1.name" class="form-control" placeholder="Name of player 1" aria-label="player1">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 ">
+                    <div class="player-container">
+                        <div class="card-body image-container">
+                            <img class="card-img-top img-fluid player-img" src="../assets/spacer2.png" />
+                        </div>
+                        <div class="footer">
+                          
+                            <input v-model="player2.name" type="text" class="form-control" placeholder="Name of player 2" aria-label="player2">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="footer">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Name of player 1"
-                aria-label="player1"
-              >
+            <div class="section-button">
+                <button type="button" @click="toggleModal" class="btn btn-success">Let's Play</button>
             </div>
-          </div>
+            <div v-if="showModal">
+                <Modal :player1='player1.name' :player2='player2.name' @close='toggleModal' />
+            </div>
         </div>
-        <div class="col-sm-6 ">
-          <div class="player-container">
-            <div class="card-body image-container">
-              <img
-                class="card-img-top img-fluid player-img"
-                src="../assets/spacer2.png"
-              />
-            </div>
-            <div class="footer">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Name of player 2"
-                aria-label="player1"
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="section-button">
-        <router-link
-          type="button"
-          class="btn btn-success"
-          data-toggle="modal"
-          data-target="#player"
-          to="/game"
-        >Let's Play</router-link>
-      </div>
+    
+    
+    
     </div>
-
-    <!-- Modal -->
-    <div
-      class="modal fade"
-      id="player"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="player-text"
-      aria-hidden="true"
-    >
-      <div
-        class="modal-dialog modal-dialog-centered"
-        role="document"
-      >
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5
-              class="modal-title"
-              id="exampleModalLongTitle"
-            >Modal title</h5>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-                </button>
-          </div>
-          <div class="modal-body">
-            kokoko
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-dismiss="modal"
-            >Close</button>
-              <button
-                type="button"
-                class="btn btn-primary"
-              >Save changes</button>
-          </div>
-        </div>
-  </div>
-  </div>
-
-  </div>
 </template>
 
 <script>
-// import Game from "./components/Game.vue";
+import Modal from "../components/welcomeModal";
 
 export default {
-  name: "App",
-  components: {},
+    name: "App",
+    components: {
+        Modal
+    },
+    data() {
+        return {
+            player1: {
+                name: null
+            },
+            player2: {
+                name: null
+            },
+            showModal: false
+        }
+    },
+    methods: {
+        toggleModal() {
+            this.showModal = !this.showModal
+        },
+    },
 };
 </script>
 
 <style scoped>
 .player-container {
-  width: 450px;
+    width: 450px;
 }
 
 .footer {
-  margin-bottom: 25px;
+    margin-bottom: 25px;
 }
 
 .player-img {
-  height: 400px;
-  width: 350px;
+    height: 400px;
+    width: 350px;
 }
 
 .image-container {
-  height: auto;
+    height: auto;
 }
 
 .player-section {
-  margin: auto;
-  align-items: center;
-  padding-top: 10px;
+    margin: auto;
+    align-items: center;
+    padding-top: 10px;
 }
 
 .section-button {
-  margin: auto;
+    margin: auto;
 }
 
 .player-container {
-  margin: auto;
+    margin: auto;
 }
 
 .screen-header {
-  padding-top: 10px;
-  padding-bottom: 10px;
-  font-family: "Open Sans", sans-serif;
-  font-weight: 300;
-  color: white;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-family: "Open Sans", sans-serif;
+    font-weight: 300;
+    color: white;
 }
 
 .btn {
-  width: 150px;
-  padding: 10px;
+    width: 150px;
+    padding: 10px;
 }
 </style>

b3871c2f9a89596a3d3e04967161ade4d8a597c7
ChadH28 <chadhoosain@gmail.com>
2021-10-04 15:22:55 +0200

    added left and right card component

---
+++ src/components/rightCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card player-card">
+        <img class="card-img-top img-fluid player-img" src="../assets/spacer2.png" alt="player1">
+        <div class="card-body">
+            <h5 class="card-title">Player 2</h5>
+            <h5 class="card-title">Score: {{player2_score}}</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'rightCard',
+    props: ['player2_score'],
+}
+</script>
+
+<style>
+.player-img {
+    height: 300px;
+    width: 250px;
+    margin: auto;
+}
+
+.player-card {
+    margin: 100px 0px 15px;
+    padding: 10px;
+    border-radius: 8px;
+    color: white;
+    background-color: rgba(255, 255, 255, 0.35);
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+}
+
+.card-title {
+    font-size: 30px;
+    font-weight: bold;
+}
+</style>
